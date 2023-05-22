3abb43812643aa4007c42df774c5fdaf7be1fd56
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-05-22 12:35:49 +0300

    Fixed line spacing on line 6, 13 and 18(Task three)

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -3,15 +3,19 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+
 @app.route('/', strict_slashes=False)
 def hello():
     '''The home page'''
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     '''The hbnb page'''
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_page(text):
