42cf2422b3ed9e31c1d5d7e6c9cc183041e85f05
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-05-22 12:42:58 +0300

    Removed quotes around port no

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -20,8 +20,8 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c_page(text):
     '''The c page'''
-    return 'c {}'.format(text.replace('_', ' '))
+    return 'C {}'.format(text.replace('_', ' '))
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
